a668794cadf0b17879ab731537760324b086d79a
mikesabat <mike.sabat@gmail.com>
2014-02-23 14:12:05 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Testless::Application.config.secret_key_base = '0aebbd4b7172427c75e334f38a5b4205f4aa043805ac7edd653729cf959c72f8e850d663702aaee11195792c0323303e5dafa2c8955109a4419dcfe71d7558b4'
