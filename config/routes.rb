7007bd8cb2687b34c7c1a4b2502d90d06d80a7ba
mikesabat <mike.sabat@gmail.com>
2014-03-23 14:21:16 -0400

    Root is pages#about

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Testless::Application.routes.draw do
 
-  get "about" => "pages#about"
+  root "pages#about"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
